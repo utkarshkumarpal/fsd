153a54b5804f70d0989b2eef8b403dd443874741
utkarshkumarpal <utkarshkumarpal78@gmail.com>
2024-11-06 00:37:59 +0530

    comitt

---
+++ day5/script.js
@@ -0,0 +1,42 @@
+// function display(){
+//     alert("Hello JS");
+// }
+// let x=12;
+
+// function display(){
+//     console.log("Hello JS");
+// }
+// let x=14;
+// console.log("Hello JS",x)
+// display();
+// function validate()
+// {
+//     let un=document.getElementById('un').value;
+//     let pass=document.getElementById('pass').value;
+//     if(un==='admin' && pass==='admin')
+//       document.getElementById("res").innerHTML = "Login Success";
+//     else
+//     document.getElementById("res").innerHTML= "Login Failed";
+
+
+// }
+
+// let a=12;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+
+// let a=(x,y) => {
+//     return (x+y);
+
+// };
+
+//  let x=a(12,34);
+//  console.log(x);
+
+
+function display(){
+    let pl = document.querySelector("p");
+    pl.innerHTML="Result";
+    pl.style.backgroundColor="red"
+}
